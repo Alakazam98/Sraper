4d0bee088b5ec7693833f6dbcb961921abc0c2e2
Alakazam98 <szymon98jalocha@gmail.com>
2019-02-08 22:29:46 +0100

    Setting mines in table

---
+++ Sraper/Sraper/GameLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sraper
+{
+    class GameLogic
+    {
+
+        readonly char[,] Board = new char[10,10];
+         Random rnd = new Random();
+
+        public void  Settingmines()
+        {
+            int minesCount = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if(minesCount==10)
+                {
+                    break;
+                }
+                for (int j = 0; j < 10; i++)
+                {
+                    int isThereMine = rnd.Next(1, 2);
+
+                    if (isThereMine == 1)
+                    {
+                        Board[i, j] = '*';
+                        minesCount++;
+                    }
+                    else
+                        continue;
+
+
+                }
+
+            }
+
+        }
+    }
+}
