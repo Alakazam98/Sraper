ef26cbff0292d34bdd84f6ec43a73c9946b5ba57
Daniel Janus <daniel.janus@gmail.com>
2019-02-20 19:43:29 +0100

    Test

---
+++ Sraper/GameLogicSraperTests/GameLogTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic.Tests
+{
+    [TestClass()]
+    public class GameLogTests
+    {
+        [TestMethod()]
+        public void CheckSurroundingTest()
+        {
+            GameLog gameLog = new GameLog();
+            int howManyMines = 0;
+            int[] RightColumn = { 1, 2, 3, 4, 5 };
+            Assert.AreEqual(1, howManyMines);
+        }
+    }
+}
